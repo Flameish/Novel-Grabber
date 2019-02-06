6e19282f1f6715802e7a989527df2eb1ec75bdcf
Flameish <flameish@pm.me>
2019-02-06 18:24:15 +0100

    Updated

--- src/fetchChapters.java
+++ src/fetchChapters.java
@@ -2,7 +2,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
