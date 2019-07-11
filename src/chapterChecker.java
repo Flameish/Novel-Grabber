808dcbe9959d63f2196a377e47a33374bd449f0e
Flameish <flameish@pm.me>
2019-07-11 02:45:24 +0200

    Cleanup

--- src/chapterChecker.java
+++ src/chapterChecker.java
@@ -1,20 +1,30 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.awt.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import static java.lang.System.out;
+import static java.lang.Math.toIntExact;
 
 class chapterChecker {
     static List<String> hosts = new ArrayList<>();
     static List<String> urls = new ArrayList<>();
+    private static List<Integer> lastChapters;
+    private static List<Integer> toBeRemoved;
     private static ScheduledExecutorService service;
     private static final int pollingInterval = 20;
     static boolean checkerRunning;
@@ -22,67 +32,19 @@ class chapterChecker {
     private static String curTitle;
     private static String latestChapterListFile;
 
+
     static void chapterPolling() {
-        if (!SystemTray.isSupported()) {
-            NovelGrabberGUI.appendText("checker", "System tray not supported!");
-            NovelGrabberGUI.stopPolling();
-            return;
-        }
-        latestChapterListFile = NovelGrabberGUI.appdataPath + File.separator + "lastChapters_" + urls.hashCode() + ".txt";
         taskIsKilled = false;
         checkerRunning = true;
-        List<Integer> lastChapter = new ArrayList<>();
-        List<Integer> toBeRemoved = new ArrayList<>();
-        NovelGrabberGUI.checkStatusLbl.setText("Initializing...");
-        //Initializing
-        //reads latest chapter numbers from existing file. File name is compared against hashCode() of List<String> urls.
+        lastChapters = new ArrayList<>();
+        toBeRemoved = new ArrayList<>();
+
+        latestChapterListFile = NovelGrabberGUI.appdataPath + File.separator + "lastChapters_" + urls.hashCode() + ".json";
         File file = new File(latestChapterListFile);
         if (file.exists()) {
-            Scanner sc = null;
-            try {
-                sc = new Scanner(new File(latestChapterListFile));
-            } catch (FileNotFoundException ex) {
-                ex.printStackTrace();
-            }
-            while (Objects.requireNonNull(sc).hasNextLine()) {
-                lastChapter.add(Integer.parseInt(sc.next()));
-                sc.nextLine();
-            }
-            sc.close();
-            //gets amount of chapters in toc of novel urls. Faulty urls get added to toBeRemoved.
+            readDataFromJSON();
         } else {
-            for (int i = 0; i < urls.size(); i++) {
-                try {
-                    NovelGrabberGUI.appendText("checker", "Initializing: " + urls.get(i));
-                    lastChapter.add(countChapters(hosts.get(i), urls.get(i)));
-                    Thread.sleep(3000);
-                } catch (IllegalArgumentException e) {
-                    toBeRemoved.add(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            //removes faulty entries
-            toBeRemoved.sort(Comparator.reverseOrder());
-            for (int a : toBeRemoved) {
-                NovelGrabberGUI.appendText("checker", "Removing faulty: " + urls.get(a));
-                urls.remove(a);
-                hosts.remove(a);
-            }
-            toBeRemoved.clear();
-            NovelGrabberGUI.listModelCheckerLinks.clear();
-            //creates a new file to store latest chapter numbers and names it after hashCode() of entries (List<String>urls
-            if (!urls.isEmpty()) {
-                latestChapterListFile = NovelGrabberGUI.appdataPath + File.separator + "lastChapters_" + urls.hashCode() + ".txt";
-                try (PrintStream out = new PrintStream(latestChapterListFile, "UTF-8")) {
-                    for (int i = 0; i < urls.size(); i++) {
-                        out.println(lastChapter.get(i) + " " + urls.get(i));
-                    }
-                    NovelGrabberGUI.appendText("checker", "Creating latest-chapter file: " + latestChapterListFile);
-                } catch (IOException e) {
-                    out.println(Shared.time() + e.getMessage());
-                }
-            }
+            initializeFile();
         }
         if (urls.isEmpty()) {
             NovelGrabberGUI.appendText("checker", "No checkers defined.");
@@ -90,33 +52,34 @@ class chapterChecker {
             NovelGrabberGUI.resetCheckerGUIButtons();
             return;
         }
-        if (lastChapter.contains(-1)) {
+        if (lastChapters.contains(-1)) {
             NovelGrabberGUI.appendText("checker", "Could not reach one or more hosts.");
+            NovelGrabberGUI.stopPolling();
+            NovelGrabberGUI.resetCheckerGUIButtons();
             return;
         }
-        //updates checker list on GUI
-        NovelGrabberGUI.listModelCheckerLinks.clear();
+        //updates checker list on GUI with chapter numbers
         for (int i = 0; i < urls.size(); i++) {
-            NovelGrabberGUI.listModelCheckerLinks.addElement("Latest chapter: " + lastChapter.get(i) + " / [" + urls.get(i) + "]");
+            NovelGrabberGUI.listModelCheckerLinks.set(i, "Latest chapter: " + lastChapters.get(i) + " / [" + urls.get(i) + "]");
         }
         Runnable runnable = () -> {
             try {
                 for (int i = 0; i < urls.size(); i++) {
                     if (!taskIsKilled) {
-                        Thread.sleep((int) (Math.random() * 7001 + 2000));
+                        Thread.sleep((int) (Math.random() * 3001 + 2000));
                         NovelGrabberGUI.appendText("checker", "Polling: " + urls.get(i));
                         int newChapter = countChapters(hosts.get(i), urls.get(i));
-                        if (newChapter > lastChapter.get(i)) {
-                            if (newChapter - lastChapter.get(i) > 1) {
-                                NovelGrabberGUI.appendText("checker", newChapter - lastChapter.get(i) + " new chapters.");
-                                showNotification(curTitle + ": " + (newChapter - lastChapter.get(i)) + " new chapters.");
+                        if (newChapter > lastChapters.get(i)) {
+                            if (newChapter - lastChapters.get(i) > 1) {
+                                NovelGrabberGUI.appendText("checker", newChapter - lastChapters.get(i) + " new chapters.");
+                                showNotification(curTitle + ": " + (newChapter - lastChapters.get(i)) + " new chapters.");
                             } else {
                                 NovelGrabberGUI.appendText("checker", "New chapter: " + newChapter);
                                 showNotification(curTitle + ": " + newChapter);
                             }
-                            modifyFile(latestChapterListFile, lastChapter.get(i).toString() + " " + urls.get(i), newChapter + " " + urls.get(i));
+                            lastChapters.add(i, newChapter);
+                            writeDataToJSON(latestChapterListFile, true);
                             NovelGrabberGUI.listModelCheckerLinks.set(i, "Latest chapter: " + newChapter + " / [" + curTitle + "]");
-                            lastChapter.add(i, newChapter);
                         }
                     }
                 }
@@ -126,9 +89,9 @@ class chapterChecker {
                 e.printStackTrace();
             }
         };
-        NovelGrabberGUI.checkStatusLbl.setText("Checking active.");
-        NovelGrabberGUI.checkStopPollingBtn.setEnabled(true);
         if (!taskIsKilled) {
+            NovelGrabberGUI.checkStatusLbl.setText("Checking active.");
+            NovelGrabberGUI.checkStopPollingBtn.setEnabled(true);
             service = Executors.newSingleThreadScheduledExecutor();
             service.scheduleAtFixedRate(runnable, 0, pollingInterval, TimeUnit.MINUTES);
         } else {
@@ -137,24 +100,63 @@ class chapterChecker {
         }
     }
 
-    private static void modifyFile(String filePath, String oldString, String newString) {
+    private static void initializeFile() {
+        NovelGrabberGUI.checkStatusLbl.setText("Initializing...");
+        //gets chapter count for each entry
+        for (int i = 0; i < urls.size(); i++) {
+            try {
+                NovelGrabberGUI.appendText("checker", "Initializing: " + urls.get(i));
+                lastChapters.add(countChapters(hosts.get(i), urls.get(i)));
+                Thread.sleep(3000);
+            } catch (IllegalArgumentException e) {
+                toBeRemoved.add(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //removes faulty entries
+        toBeRemoved.sort(Comparator.reverseOrder());
+        for (int a : toBeRemoved) {
+            NovelGrabberGUI.appendText("checker", "Removing faulty: " + urls.get(a));
+            urls.remove(a);
+            hosts.remove(a);
+            NovelGrabberGUI.listModelCheckerLinks.removeElementAt(a);
+        }
+        toBeRemoved.clear();
+        //creates a new file to store latest chapter numbers and names it after hashCode() of entries (List<String>urls
+        if (!urls.isEmpty()) {
+            latestChapterListFile = NovelGrabberGUI.appdataPath + File.separator + "lastChapters_" + urls.hashCode() + ".json";
+            writeDataToJSON(latestChapterListFile, true);
+        }
+    }
+
+    //reads latest chapter numbers from existing file
+    private static void readDataFromJSON() {
+        JSONParser parser = new JSONParser();
         try {
-            File file = new File(filePath);
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
-            StringBuilder oldText = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                oldText.append(line).append(System.lineSeparator());
+            JSONArray a = (JSONArray) parser.parse(new FileReader(latestChapterListFile));
+            for (Object o : a) {
+                JSONObject checker = (JSONObject) o;
+                lastChapters.add(toIntExact((Long) checker.get("CHAPTER")));
             }
-            reader.close();
-            String newText = oldText.toString().replaceAll(oldString, newString);
-            try (PrintStream out = new PrintStream(filePath, "UTF-8")) {
-                out.print(newText);
-            } catch (IOException e) {
-                out.println(Shared.time() + e.getMessage());
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void writeDataToJSON(String filepath, boolean withChapters) {
+        JSONArray array = new JSONArray();
+        for (int i = 0; i < hosts.size(); i++) {
+            JSONObject checker = new JSONObject();
+            if (withChapters) checker.put("CHAPTER", lastChapters.get(i));
+            checker.put("HOST", hosts.get(i));
+            checker.put("URL", urls.get(i));
+            array.add(checker);
+        }
+        try (FileWriter JSONfile = new FileWriter(filepath)) {
+            JSONfile.write(array.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -176,13 +178,11 @@ class chapterChecker {
 
     private static int countChapters(String host, String tocUrl) {
         Novel currentNovel = new Novel(host, tocUrl);
-        Document doc = null;
         try {
-            doc = Jsoup.connect(currentNovel.getUrl()).timeout(10 * 1000).get();
+            Document doc = Jsoup.connect(currentNovel.getUrl()).timeout(30 * 1000).get();
             curTitle = doc.title();
-            Elements content = doc.select(currentNovel.getChapterLinkContainer());
-            Elements chapterItem = content.select(currentNovel.getChapterLinkSelector());
-            return chapterItem.size();
+            Elements chapterItems = doc.select(currentNovel.getChapterLinkSelector());
+            return chapterItems.size();
         } catch (IOException e) {
             e.printStackTrace();
             return -1;
