594fd459de7097b93116a0e51b555f0249dd54bb
Flameish <https://github.com/join>
2020-07-14 16:22:38 +0200

    Add more settings

--- src/library/LibrarySystem.java
+++ src/library/LibrarySystem.java
@@ -13,54 +13,59 @@ import java.util.concurrent.Executors;
 public class LibrarySystem {
 
     public static void startPolling() {
-        // Start new thread not to block others
         Executors.newSingleThreadExecutor().execute(LibrarySystem::pollLibrary);
     }
 
     private static void pollLibrary() {
-        System.out.println("[INFO]Polling library...");
-        for(String novelUrl: Library.getLibrary()) {
-            // Getting the latest chapter count
-            NovelOptions options = new NovelOptions();
-            options.hostname = Library.getHost(novelUrl);
-            options.headless = Library.useHeadless(novelUrl);
-            options.useAccount = Library.useAccount(novelUrl);
-            options.window  = "checker";
-            options.novelLink = novelUrl;
+        if(Library.getPolling()) {
+            Notification mailer = new Notification();
+            System.out.println("[INFO]Polling library...");
+            for(String novelUrl: Library.getLibrary()) {
+                // Getting the latest chapter count
+                NovelOptions options = new NovelOptions();
+                options.hostname = Library.getHost(novelUrl);
+                options.headless = Library.useHeadless(novelUrl);
+                options.useAccount = Library.useAccount(novelUrl);
+                options.window  = "checker";
+                options.novelLink = novelUrl;
 
-            Novel autoNovel = new Novel(options);
-            autoNovel.getChapterList();
-            autoNovel.getMetadata();
-            Library.setNewestChapter(novelUrl, autoNovel.chapters.size());
-
-            if(Library.getNewestChapter(novelUrl) - Library.getLastChapter(novelUrl) >= Library.getThreshold(novelUrl)) {
+                Novel autoNovel = new Novel(options);
+                autoNovel.getChapterList();
+                autoNovel.getMetadata();
+                Library.setNewestChapter(novelUrl, autoNovel.chapters.size());
+                int chapterDifference = Library.getNewestChapter(novelUrl) - Library.getLastChapter(novelUrl);
                 // Autodownload
-                if(Library.getAutoDownload(novelUrl)) {
-                    String cliString = Library.getCLICommand(novelUrl)
-                            +" -window checker -chapters "
-                            +(Library.getLastChapter(novelUrl)+1)
-                            +" "
-                            +Library.getNewestChapter(novelUrl);
-                    String[] cliParams = cliString.split(" ");
-                    autoNovel = init.processParams(init.getParamsFromString(cliParams));
-                    Library.setLastChapter(novelUrl, Library.getNewestChapter(novelUrl));
+                if(chapterDifference >= Library.getThreshold(novelUrl) && chapterDifference > 0) {
+                    if(Library.getAutoDownload(novelUrl)) {
+                        String cliString = Library.getCLICommand(novelUrl)
+                                +" -window checker -chapters "
+                                +(Library.getLastChapter(novelUrl)+1)
+                                +" "
+                                +Library.getNewestChapter(novelUrl);
+                        String[] cliParams = cliString.split(" ");
+                        autoNovel = init.processParams(init.getParamsFromString(cliParams));
+                        Library.setLastChapter(novelUrl, Library.getNewestChapter(novelUrl));
+                        if(EmailConfig.useAttachment() && !EmailConfig.getHost().isEmpty()) {
+                            mailer.sendAttachment(autoNovel);
+                            System.out.println("[INFO]Email with attachment send.");
+                        }
+                    }
                 }
-                if(EmailConfig.useAttachment() && Library.getAutoDownload(novelUrl)) {
-                    Notification.sendAttachment(autoNovel);
-                    System.out.println("[INFO]Email with attachment send.");
-                }
-                if(EmailConfig.useNotifications()) {
-                    Notification.sendNotification(autoNovel);
+                // Notification
+                if(EmailConfig.useNotifications() && chapterDifference > 0 && !EmailConfig.getHost().isEmpty()) {
+                    mailer.sendNotification(autoNovel);
                     System.out.println("[INFO]Notification send.");
+                    // Adjust last downloaded chapter to newest
+                    if(Library.getUpdateLast()) {
+                        Library.setLastChapter(novelUrl, autoNovel.chapters.size());
+                    }
                 }
             }
-
-            GrabberUtils.sleep(1000);
+            System.out.println("[INFO]Polling done.");
+            init.window.buildLibrary();
+            // wait 20 minutes
         }
-        System.out.println("[INFO]Polling done.");
-        init.window.buildLibrary();
-        // wait 20 minutes
-        GrabberUtils.sleep(20*60*1000);
+        GrabberUtils.sleep(Library.getFrequency()*60*1000);
         pollLibrary();
     }
 }
