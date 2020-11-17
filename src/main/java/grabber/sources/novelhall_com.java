7fcd14ea932d603fca4155a5d143ab9f8dd0bc40
Flameish <flameish@pm.me>
2020-11-17 19:06:40 +0100

    Add novelhall.com support

---
+++ src/main/java/grabber/sources/novelhall_com.java
@@ -0,0 +1,82 @@
+package grabber.sources;
+
+import grabber.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import system.init;
+
+public class novelhall_com implements Source {
+    private final Novel novel;
+    private Document toc;
+
+    public novelhall_com(Novel novel) {
+        this.novel = novel;
+    }
+
+    public List<Chapter> getChapterList() {
+        List<Chapter> chapterList = new ArrayList();
+        try {
+            toc = Jsoup.connect(novel.novelLink).get();
+            Elements chapterLinks = toc.select("#morelist li a");
+            for(Element chapterLink: chapterLinks) {
+                chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (init.gui != null) {
+                init.gui.appendText(novel.window, "[ERROR]Could not connect to webpage. (" + e.getMessage() + ")");
+            }
+        }
+        return chapterList;
+    }
+
+    public Element getChapterContent(Chapter chapter) {
+        Element chapterBody = null;
+        try {
+            Document doc = Jsoup.connect(chapter.chapterURL).get();
+            chapterBody = doc.select(".entry-content").first();
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (init.gui != null) {
+                init.gui.appendText(novel.window, "[ERROR]Could not connect to webpage. (" + e.getMessage() + ")");
+            }
+        }
+        return chapterBody;
+    }
+
+    public NovelMetadata getMetadata() {
+        NovelMetadata metadata = new NovelMetadata();
+
+        metadata.setTitle(toc.select("meta[property=og:title]").attr("content"));
+        metadata.setAuthor(toc.select("meta[property=books:author]").attr("content"));
+        String description = toc.select(".js-close-wrap").first().text().replace("back<<","");
+        metadata.setDescription(description);
+        metadata.setBufferedCover(toc.select("meta[property=og:image]").attr("abs:content"));
+        Elements tags = toc.select(".booktag .red");
+        List<String> subjects = new ArrayList<>();
+        for(Element tag: tags) {
+            subjects.add(tag.text());
+        }
+        metadata.setSubjects(subjects);
+
+        return metadata;
+    }
+
+    public List<String> getBlacklistedTags() {
+        List blacklistedTags = new ArrayList();
+        return blacklistedTags;
+    }
+
+    public Map<String, String> getLoginCookies() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+}
