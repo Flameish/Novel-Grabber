2b2167f6f054b57e41016e1e536700339fc6173d
Flameish <flameish@pm.me>
2020-12-02 17:05:54 +0100

    Added

---
+++ src/main/java/grabber/sources/boxnovel_net.java
@@ -0,0 +1,154 @@
+package grabber.sources;
+
+import grabber.Chapter;
+import grabber.Novel;
+import grabber.NovelMetadata;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import system.init;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class boxnovel_net implements Source {
+    private final Novel novel;
+    private Document toc;
+
+    public boxnovel_net(Novel novel) {
+        this.novel = novel;
+    }
+
+    public List<Chapter> getChapterList() {
+        List<Chapter> chapterList = new ArrayList();
+        try {
+            toc = Jsoup.connect(novel.novelLink)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            Elements chapterLinks;
+            while (!toc.select("#navigation-ajax:contains(NEXT PAGE)").isEmpty()) {
+                chapterLinks = toc.select(".wp-manga-chapter a");
+                for (Element chapterLink : chapterLinks) {
+                    chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
+                }
+                toc = Jsoup.connect(toc.select("#navigation-ajax:contains(NEXT PAGE)").attr("abs:href"))
+                        .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                        .get();
+            }
+            chapterLinks = toc.select(".wp-manga-chapter a");
+            for (Element chapterLink : chapterLinks) {
+                chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
+            }
+            Collections.reverse(chapterList);
+        } catch (HttpStatusException httpEr) {
+            String errorMsg;
+            int errorCode = httpEr.getStatusCode();
+            switch(errorCode) {
+                case 403:
+                    errorMsg = "[ERROR] Forbidden! (403)";
+                    break;
+                case 404:
+                    errorMsg = "[ERROR] Page not found! (404)";
+                    break;
+                case 500:
+                    errorMsg = "[ERROR] Server error! (500)";
+                    break;
+                case 503:
+                    errorMsg = "[ERROR] Service Unavailable! (503)";
+                    break;
+                case 504:
+                    errorMsg = "[ERROR] Gateway Timeout! (504)";
+                    break;
+                default:
+                    errorMsg = "[ERROR] Could not connect to webpage!";
+            }
+            System.err.println(errorMsg);
+            if (init.gui != null) {
+                init.gui.appendText(novel.window, errorMsg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (init.gui != null) {
+                init.gui.appendText(novel.window, "[ERROR] Could not connect to webpage!");
+            }
+        }
+        return chapterList;
+    }
+
+    public Element getChapterContent(Chapter chapter) {
+        Element chapterBody = null;
+        try {
+            Document doc = Jsoup.connect(chapter.chapterURL).get();
+            chapterBody = doc.select(".text-content").first();
+        } catch (HttpStatusException httpEr) {
+            String errorMsg;
+            int errorCode = httpEr.getStatusCode();
+            switch(errorCode) {
+                case 403:
+                    errorMsg = "[ERROR] Forbidden! (403)";
+                    break;
+                case 404:
+                    errorMsg = "[ERROR] Page not found! (404)";
+                    break;
+                case 500:
+                    errorMsg = "[ERROR] Server error! (500)";
+                    break;
+                case 503:
+                    errorMsg = "[ERROR] Service Unavailable! (503)";
+                    break;
+                case 504:
+                    errorMsg = "[ERROR] Gateway Timeout! (504)";
+                    break;
+                default:
+                    errorMsg = "[ERROR] Could not connect to webpage!";
+            }
+            System.err.println(errorMsg);
+            if (init.gui != null) {
+                init.gui.appendText(novel.window, errorMsg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (init.gui != null) {
+                init.gui.appendText(novel.window, "[ERROR] Could not connect to webpage!");
+            }
+        }
+        return chapterBody;
+    }
+
+    public NovelMetadata getMetadata() {
+        NovelMetadata metadata = new NovelMetadata();
+
+        if(toc != null) {
+            metadata.setTitle(toc.select(".post-title").first().text());
+            metadata.setAuthor(toc.select(".author-content").first().text());
+            metadata.setDescription(toc.select(".description-summary").first().text());
+            metadata.setBufferedCover(toc.select(".summary_image img").attr("abs:src"));
+
+            Elements tags = toc.select(".genres-content a");
+            List<String> subjects = new ArrayList<>();
+            for(Element tag: tags) {
+                subjects.add(tag.text());
+            }
+            metadata.setSubjects(subjects);
+        }
+
+        return metadata;
+    }
+
+    public List<String> getBlacklistedTags() {
+        List blacklistedTags = new ArrayList();
+        blacklistedTags.add("div.code-block");
+        blacklistedTags.add(".adbox");
+        return blacklistedTags;
+    }
+
+    public Map<String, String> getLoginCookies() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+}
