1ee42d493186dddc682814f647077abc4acbb1cd
Flameish <flameish@pm.me>
2020-12-03 20:28:38 +0100

    Added

---
+++ src/main/java/grabber/sources/readlightnovel_org.java
@@ -0,0 +1,114 @@
+package grabber.sources;
+
+import grabber.Chapter;
+import grabber.Novel;
+import grabber.NovelMetadata;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import system.init;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class readlightnovel_org implements Source {
+    private final Novel novel;
+    private Document toc;
+
+    public readlightnovel_org(Novel novel) {
+        this.novel = novel;
+    }
+
+    public List<Chapter> getChapterList() {
+        List<Chapter> chapterList = new ArrayList();
+        try {
+            toc = Jsoup.connect(novel.novelLink)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            Elements chapterLinks = toc.select(".chapter-chs a");
+            for(Element chapterLink: chapterLinks) {
+                chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return chapterList;
+    }
+
+
+    public Element getChapterContent(Chapter chapter) {
+        Element chapterBody = null;
+        try {
+            Document doc = Jsoup.connect(chapter.chapterURL)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            chapterBody = doc.select(".desc").first();
+        } catch (HttpStatusException httpEr) {
+            String errorMsg;
+            int errorCode = httpEr.getStatusCode();
+            switch(errorCode) {
+                case 403:
+                    errorMsg = "[ERROR] Forbidden! (403)";
+                    break;
+                case 404:
+                    errorMsg = "[ERROR] Page not found! (404)";
+                    break;
+                case 500:
+                    errorMsg = "[ERROR] Server error! (500)";
+                    break;
+                case 503:
+                    errorMsg = "[ERROR] Service Unavailable! (503)";
+                    break;
+                case 504:
+                    errorMsg = "[ERROR] Gateway Timeout! (504)";
+                    break;
+                default:
+                    errorMsg = "[ERROR] Could not connect to webpage!";
+            }
+            System.err.println(errorMsg);
+            if (init.gui != null) {
+                init.gui.appendText(novel.window, errorMsg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (init.gui != null) {
+                init.gui.appendText(novel.window, "[ERROR] Could not connect to webpage!");
+            }
+        }
+        return chapterBody;
+    }
+
+    public NovelMetadata getMetadata() {
+        NovelMetadata metadata = new NovelMetadata();
+
+        if(toc != null) {
+            metadata.setTitle(toc.select(".block-title h1").first().text());
+            metadata.setDescription(toc.select(".novel-detail-body").first().text());
+            metadata.setBufferedCover(toc.select(".novel-cover img").attr("abs:src"));
+
+            Elements tags = toc.select("div.novel-details:nth-child(4) > div:nth-child(2) > div:nth-child(2) > ul:nth-child(1) a");
+            List<String> subjects = new ArrayList<>();
+            for(Element tag: tags) {
+                subjects.add(tag.text());
+            }
+            metadata.setSubjects(subjects);
+        }
+
+        return metadata;
+    }
+
+    public List<String> getBlacklistedTags() {
+        List blacklistedTags = new ArrayList();
+        blacklistedTags.add(".apester-media");
+        return blacklistedTags;
+    }
+
+    public Map<String, String> getLoginCookies() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+}
