d431281d05c9d5a8544769cbcd53b42d94d86ea0
Flameish <flameish@pm.me>
2020-12-03 17:49:49 +0100

    Added login for LNMTL

--- src/main/java/grabber/sources/lnmtl_com.java
+++ src/main/java/grabber/sources/lnmtl_com.java
@@ -19,6 +19,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import system.data.accounts.Account;
+import system.data.accounts.Accounts;
 import system.init;
 
 public class lnmtl_com implements Source {
@@ -105,9 +107,17 @@ public class lnmtl_com implements Source {
     public Element getChapterContent(Chapter chapter) {
         Element chapterBody = null;
         try {
-            Document doc = Jsoup.connect(chapter.chapterURL)
-                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
-                    .get();
+            Document doc;
+            if(novel.cookies != null) {
+                doc = Jsoup.connect(chapter.chapterURL)
+                        .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                        .cookies(novel.cookies)
+                        .get();
+            } else {
+                doc = Jsoup.connect(chapter.chapterURL)
+                        .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                        .get();
+            }
             Elements unfixedSentences = doc.select(".translated");
             StringBuilder fixedChapter = new StringBuilder();
             for(Element sentence: unfixedSentences) {
@@ -175,6 +185,37 @@ public class lnmtl_com implements Source {
     }
 
     public Map<String, String> getLoginCookies() throws UnsupportedOperationException {
+        System.out.println("[INFO] Login...");
+        if(init.gui != null) {
+            init.gui.appendText(novel.window,"[INFO] Login...");
+        }
+        try {
+            Account account = Accounts.getInstance().getAccount("LNMTL");
+            if(!account.getUsername().isEmpty()) {
+                Connection.Response res = Jsoup.connect("https://lnmtl.com/auth/login")
+                        .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                        .method(Connection.Method.GET)
+                        .execute();
+                String token = res.parse().select("input[name=_token]").attr("value");
+                res = Jsoup.connect("https://lnmtl.com/auth/login")
+                        .data("email", account.getUsername())
+                        .data("password", account.getPassword())
+                        .data("_token", token)
+                        .cookies(res.cookies())
+                        .method(Connection.Method.POST)
+                        .execute();
+                return res.cookies();
+            } else {
+                System.out.println("[ERROR] No account found.");
+                if(init.gui != null) {
+                    init.gui.appendText(novel.window,"[ERROR] No account found.");
+                }
+                return null;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         throw new UnsupportedOperationException();
     }
 
