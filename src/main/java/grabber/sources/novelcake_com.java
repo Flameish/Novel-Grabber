43c23655fe184031f7cb66911a843cf3101db4bd
Flameish <flameish@pm.me>
2021-02-13 22:09:27 +0100

    Added new site

---
+++ src/main/java/grabber/sources/novelcake_com.java
@@ -0,0 +1,117 @@
+package grabber.sources;
+
+import grabber.Chapter;
+import grabber.GrabberUtils;
+import grabber.Novel;
+import grabber.NovelMetadata;
+import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class novelcake_com implements Source {
+    private final String name = "NovelCake";
+    private final String url = "https://novelcake.com/";
+    private final boolean canHeadless = false;
+    private Novel novel;
+    private Document toc;
+
+    public novelcake_com() {
+    }
+
+    public novelcake_com(Novel novel) {
+        this.novel = novel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean canHeadless() {
+        return canHeadless;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public List<Chapter> getChapterList() {
+        List<Chapter> chapterList = new ArrayList();
+        try {
+            toc = Jsoup.connect(novel.novelLink).cookies(novel.cookies).get();
+            Connection.Response res = Jsoup.connect("https://novelcake.com/wp-admin/admin-ajax.php")
+                    .cookies(novel.cookies)
+                    .data("action","manga_get_chapters")
+                    .data("manga",toc.selectFirst(".wp-manga-action-button").attr("data-post"))
+                    .method(Connection.Method.POST)
+                    .execute();
+            Elements chapterLinks = res.parse().select(".wp-manga-chapter a");
+            for (Element chapterLink : chapterLinks) {
+                chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
+            }
+            Collections.reverse(chapterList);
+        } catch (HttpStatusException httpEr) {
+            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+        } catch (IOException e) {
+            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+        }
+        return chapterList;
+    }
+
+    public Element getChapterContent(Chapter chapter) {
+        Element chapterBody = null;
+        try {
+            Document doc = Jsoup.connect(chapter.chapterURL).cookies(novel.cookies).get();
+            chapterBody = doc.select(".text-left").first();
+        } catch (HttpStatusException httpEr) {
+            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+        } catch (IOException e) {
+            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+        }
+        return chapterBody;
+    }
+
+    public NovelMetadata getMetadata() {
+        NovelMetadata metadata = new NovelMetadata();
+
+        if (toc != null) {
+            Element title = toc.selectFirst(".post-title h1");
+            Element author = toc.selectFirst(".author-content");
+            Element desc = toc.selectFirst(".description-summary");
+            Element cover = toc.selectFirst("meta[property=og:image]");
+
+            metadata.setTitle(title != null ? title.text() : "");
+            metadata.setAuthor(author != null ? author.text() : "");
+            metadata.setDescription(desc != null ? desc.text() : "");
+            metadata.setBufferedCover(cover != null ? cover.attr("content") : "");
+
+            Elements tags = toc.select(".genres-content a");
+            List<String> subjects = new ArrayList<>();
+            for (Element tag : tags) {
+                subjects.add(tag.text());
+            }
+            metadata.setSubjects(subjects);
+        }
+
+        return metadata;
+    }
+
+    public List<String> getBlacklistedTags() {
+        List blacklistedTags = new ArrayList();
+        blacklistedTags.add("div.code-block");
+        blacklistedTags.add(".adbox");
+        return blacklistedTags;
+    }
+
+}
