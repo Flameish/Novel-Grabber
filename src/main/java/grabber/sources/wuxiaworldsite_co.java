59b9e89ea946febdcc2b6f2d7a4e3315a401b6ba
Flameish <flameish@pm.me>
2021-05-16 01:05:54 +0200

    Add wuxiaworldsite.co

---
+++ src/main/java/grabber/sources/wuxiaworldsite_co.java
@@ -0,0 +1,117 @@
+package grabber.sources;
+
+import grabber.Chapter;
+import grabber.GrabberUtils;
+import grabber.Novel;
+import grabber.NovelMetadata;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class wuxiaworldsite_co implements Source {
+    private final String name = "WuxiaWorldSite.co";
+    private final String url = "https://wuxiaworldsite.co/";
+    private final boolean canHeadless = false;
+    private Novel novel;
+    private Document toc;
+
+    public wuxiaworldsite_co() {
+    }
+
+    public wuxiaworldsite_co(Novel novel) {
+        this.novel = novel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean canHeadless() {
+        return canHeadless;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public List<Chapter> getChapterList() {
+        List<Chapter> chapterList = new ArrayList();
+        try {
+            toc = Jsoup.connect(novel.novelLink)
+                    .cookies(novel.cookies)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            String novelId = toc.selectFirst(".show-more-list").attr("data-id");
+            Document chapterListPage = Jsoup.connect("https://wuxiaworldsite.co/get-full-list.ajax?id=" + novelId).get();
+            Elements chapterLinks = chapterListPage.select("a");
+            for (Element chapterLink : chapterLinks) {
+                chapterLink.select("i").remove();
+                chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
+            }
+        } catch (HttpStatusException httpEr) {
+            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+        } catch (IOException e) {
+            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+        }
+        return chapterList;
+    }
+
+    public Element getChapterContent(Chapter chapter) {
+        Element chapterBody = null;
+        try {
+            Document doc = Jsoup.connect(chapter.chapterURL)
+                    .cookies(novel.cookies)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            chapterBody = doc.selectFirst(".content-story");
+        } catch (HttpStatusException httpEr) {
+            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+        } catch (IOException e) {
+            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+        }
+        return chapterBody;
+    }
+
+    public NovelMetadata getMetadata() {
+        NovelMetadata metadata = new NovelMetadata();
+
+        if (toc != null) {
+            Element title = toc.selectFirst("meta[property=og:site_name]");
+            Element author = toc.selectFirst(".content-reading:has(h1.heading_read) p:has(i.fa-user)");
+            Element desc = toc.selectFirst(".story-introduction-content");
+            Element cover = toc.selectFirst("meta[property=og:image]");
+
+            metadata.setTitle(title != null ? title.attr("content") : "");
+            metadata.setAuthor(author != null ? author.text() : "");
+            metadata.setDescription(desc != null ? desc.text() : "");
+            metadata.setBufferedCover(cover != null ? cover.attr("abs:content") : "");
+
+            Elements tags = toc.select(".content-reading:has(h1.heading_read) .tags a");
+            List<String> subjects = new ArrayList<>();
+            for (Element tag : tags) {
+                subjects.add(tag.text());
+            }
+            metadata.setSubjects(subjects);
+        }
+
+        return metadata;
+    }
+
+    public List<String> getBlacklistedTags() {
+        List blacklistedTags = new ArrayList();
+        blacklistedTags.add("p[style=display: none]");
+        blacklistedTags.add("p[style=display:none]");
+        return blacklistedTags;
+    }
+
+}
