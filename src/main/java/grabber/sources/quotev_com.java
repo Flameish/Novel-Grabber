b00c4ef6420643d2ef30d8b993804e024be553e3
Flameish <flameish@pm.me>
2021-05-20 21:29:20 +0200

    Updated to use headless browser

--- src/main/java/grabber/sources/quotev_com.java
+++ src/main/java/grabber/sources/quotev_com.java
@@ -1,18 +1,20 @@
 package grabber.sources;
 
-import grabber.Chapter;
-import grabber.GrabberUtils;
-import grabber.Novel;
-import grabber.NovelMetadata;
+import grabber.*;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class quotev_com implements Source {
     private final String name = "Quotev";
@@ -47,44 +49,59 @@ public class quotev_com implements Source {
     public List<Chapter> getChapterList() {
         List<Chapter> chapterList = new ArrayList();
         try {
-            toc = Jsoup.connect(novel.novelLink).get();
+            toc = getTocHeadless();
             Elements chapterLinks = toc.select("#rselectList a");
             for (Element chapterLink : chapterLinks) {
                 chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
             }
-        } catch (HttpStatusException httpEr) {
-            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
-        } catch (IOException e) {
-            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
         } catch (NullPointerException e) {
             GrabberUtils.err(novel.window, "Could not find expected selectors. Correct novel link?", e);
         }
         return chapterList;
     }
 
+    private Document getTocHeadless() {
+        if (novel.headlessDriver == null) novel.headlessDriver = new Driver(novel.window);
+        novel.headlessDriver.driver.navigate().to(novel.novelLink);
+        novel.cookies.forEach((key, value) -> novel.headlessDriver.driver.manage().addCookie(new Cookie(key, value)));
+        novel.headlessDriver.driver.navigate().to(novel.novelLink);
+        String baseUrl = novel.headlessDriver.driver.getCurrentUrl().substring(0, GrabberUtils.ordinalIndexOf(novel.headlessDriver.driver.getCurrentUrl(), "/", 3) + 1);
+        Document toc = Jsoup.parse(novel.headlessDriver.driver.getPageSource(), baseUrl);
+        novel.headlessDriver.driver.close();
+        novel.headlessDriver = null;
+        return toc;
+    }
+
     public Element getChapterContent(Chapter chapter) {
-        Element chapterBody = null;
-        try {
-            Document doc = Jsoup.connect(chapter.chapterURL).get();
-            chapterBody = doc.select("#rescontent").first();
-        } catch (HttpStatusException httpEr) {
-            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
-        } catch (IOException e) {
-            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
-        }
-        return chapterBody;
+        Document doc = getPageHeadless(chapter);
+        return doc.select("#rescontent").first();
+    }
+
+    private Document getPageHeadless(Chapter chapter) {
+        if (novel.headlessDriver == null) novel.headlessDriver = new Driver(novel.window);
+        novel.headlessDriver.driver.navigate().to(chapter.chapterURL);
+        novel.cookies.forEach((key, value) -> novel.headlessDriver.driver.manage().addCookie(new Cookie(key, value)));
+        novel.headlessDriver.driver.navigate().to(chapter.chapterURL);
+        WebElement chapterElement = novel.headlessDriver.driver.findElement(By.cssSelector("body"));
+        String baseUrl = novel.headlessDriver.driver.getCurrentUrl().substring(0, GrabberUtils.ordinalIndexOf(novel.headlessDriver.driver.getCurrentUrl(), "/", 3) + 1);
+        return Jsoup.parse(chapterElement.getAttribute("innerHTML"), baseUrl);
     }
 
     public NovelMetadata getMetadata() {
         NovelMetadata metadata = new NovelMetadata();
 
         if (toc != null) {
-            metadata.setTitle(toc.select("#quizHeaderTitle h1").first().text());
-            metadata.setAuthor(toc.select("a[rel=author]").first().text());
-            metadata.setDescription(toc.select("#qdesct").first().text());
-            metadata.setBufferedCover(toc.select(".logo").attr("abs:src"));
+            Element title = toc.selectFirst("title");
+            Element author = toc.selectFirst(".quizAuthorList a");
+            Element desc = toc.selectFirst("#qdesct");
+            Element cover = toc.selectFirst("meta[property=og:image]");
 
-            Elements tags = toc.select(".genres");
+            metadata.setTitle(title != null ? title.text() : "");
+            metadata.setAuthor(author != null ? author.text() : "");
+            metadata.setDescription(desc != null ? desc.text() : "");
+            metadata.setBufferedCover(cover != null ? cover.attr("abs:content") : "");
+
+            Elements tags = toc.select("#quizViewTagTop a");
             List<String> subjects = new ArrayList<>();
             for (Element tag : tags) {
                 subjects.add(tag.text());
