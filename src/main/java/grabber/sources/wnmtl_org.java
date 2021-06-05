199e3704b11803f167b4c157ce8d2c5da55665f4
Flameish <flameish@pm.me>
2021-06-05 12:07:55 +0200

    Fixed formatting

--- src/main/java/grabber/sources/wnmtl_org.java
+++ src/main/java/grabber/sources/wnmtl_org.java
@@ -4,7 +4,6 @@ import grabber.Chapter;
 import grabber.GrabberUtils;
 import grabber.Novel;
 import grabber.NovelMetadata;
-
 import org.jsoup.Connection;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
@@ -17,9 +16,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class wnmtl_org implements Source{
+public class wnmtl_org implements Source {
     private final String name = "WNMTL";
-    private final String url = "https://wnmtl.org";
+    private final String url = "https://www.wnmtl.org/";
     private final boolean canHeadless = false;
     private Novel novel;
     private Document toc;
@@ -27,95 +26,94 @@ public class wnmtl_org implements Source{
     public wnmtl_org(Novel novel) {
         this.novel = novel;
     }
-    public wnmtl_org() {}
 
-    @Override
+    public wnmtl_org() {
+    }
+
     public String getName() {
         return name;
     }
 
-    @Override
-    public String getUrl() {
-        return url;
-    }
-
-    @Override
     public boolean canHeadless() {
         return canHeadless;
     }
 
-    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
     public List<Chapter> getChapterList() {
-        List<Chapter> cList = new ArrayList<>();
+        List<Chapter> chapterList = new ArrayList();
         try {
-            toc = Jsoup.connect(novel.novelLink)
-                    .cookies(novel.cookies)
-                    .get();
-            Connection.Response res = Jsoup.connect("https://www.wnmtl.org/wp-admin/admin-ajax.php")
-                    .data("action","manga_get_chapters")
-                    .data("manga",toc.select("div.post-rating > input").attr("value"))
+            toc = Jsoup.connect(novel.novelLink).cookies(novel.cookies).get();
+            Connection.Response res = Jsoup.connect("https://wnmtl.org/wp-admin/admin-ajax.php")
+                    .data("action", "manga_get_chapters")
+                    .data("manga", toc.select(".rating-post-id").attr("value"))
                     .cookies(novel.cookies)
                     .method(Connection.Method.POST)
                     .execute();
-            Elements chaps = res.parse().select("div.listing-chapters_wrap show-more > ul > li > a");
-            for (Element e : chaps) {
-                cList.add(new Chapter(e.text(), e.attr("abs:href")));
+            Elements chapterLinks = res.parse().select(".listing-chapters_wrap a");
+            for (Element chapterLink : chapterLinks) {
+                chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
             }
-            Collections.reverse(chaps);
-        }catch (HttpStatusException httpEr) {
+            Collections.reverse(chapterList);
+        } catch (HttpStatusException httpEr) {
             GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
         } catch (IOException e) {
             GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
         } catch (NullPointerException e) {
             GrabberUtils.err(novel.window, "Could not find expected selectors. Correct novel link?", e);
         }
-        return cList;
+        return chapterList;
     }
 
-    @Override
     public Element getChapterContent(Chapter chapter) {
-        Element body = null;
+        Element chapterBody = null;
         try {
-            Document doc = Jsoup.connect(chapter.chapterURL)
-                    .cookies(novel.cookies)
-                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
-                    .get();
-            body = doc.selectFirst("div.text-left");
+            Document doc = Jsoup.connect(chapter.chapterURL).cookies(novel.cookies).get();
+            chapterBody = doc.select(".text-left").first();
         } catch (HttpStatusException httpEr) {
             GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
         } catch (IOException e) {
             GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
         }
-        return body;
+        return chapterBody;
     }
 
-    @Override
     public NovelMetadata getMetadata() {
-        NovelMetadata mdata = new NovelMetadata();
-        if (toc != null) {
-                Element title = toc.selectFirst("div.post-title > h1");
-                Element author = toc.selectFirst("div.author-content > a");
-                Element description = toc.selectFirst("div.description-summary > div > p");
-                Element cimg = toc.selectFirst("img.img-responsive");
-                mdata.setTitle(title != null ? title.text() : "");
-                mdata.setAuthor(author != null ? author.text() : "");
-                mdata.setDescription(description != null ? description.text() : "");
-                mdata.setBufferedCover(cimg != null ? cimg.attr("abs:src") : "");
-                Elements tags = toc.select("div.genres-content > a");
-                List<String> subjects = new ArrayList<>();
-                for (Element tag : tags) {
-                    subjects.add(tag.text());
-                }
-                mdata.setSubjects(subjects);
+        NovelMetadata metadata = new NovelMetadata();
 
+        if (toc != null) {
+            Element title = toc.selectFirst(".post-title");
+            Element author = toc.selectFirst(".author-content");
+            Element desc = toc.selectFirst(".summary__content");
+            Element cover = toc.selectFirst("meta[property=og:image]");
+
+            metadata.setTitle(title != null ? title.text() : "");
+            metadata.setAuthor(author != null ? author.text() : "");
+            metadata.setDescription(desc != null ? desc.text() : "");
+            metadata.setBufferedCover(cover != null ? cover.attr("content") : "");
+
+            Elements tags = toc.select(".genres-content a");
+            List<String> subjects = new ArrayList<>();
+            for (Element tag : tags) {
+                subjects.add(tag.text());
+            }
+            metadata.setSubjects(subjects);
         }
-        return mdata;
+
+        return metadata;
     }
 
-    @Override
     public List<String> getBlacklistedTags() {
-        List<String> blacklistedTags = new ArrayList<>();
-        blacklistedTags.add("div.nav-links");
+        List blacklistedTags = new ArrayList();
+        blacklistedTags.add("div.code-block");
+        blacklistedTags.add(".adbox");
         return blacklistedTags;
     }
+
 }
