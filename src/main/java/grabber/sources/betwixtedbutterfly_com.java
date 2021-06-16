8f9766a15ac43747461a10d1ced6836cc155833c
Flameish <flameish@pm.me>
2021-06-16 12:09:14 +0200

    Added Betwixted Translations

---
+++ src/main/java/grabber/sources/betwixtedbutterfly_com.java
@@ -0,0 +1,118 @@
+package grabber.sources;
+
+import grabber.Chapter;
+import grabber.GrabberUtils;
+import grabber.Novel;
+import grabber.NovelMetadata;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class betwixtedbutterfly_com implements Source {
+    private final String name = "Betwixted Translations";
+    private final String url = "https://betwixtedbutterfly.com/";
+    private final boolean canHeadless = false;
+    private Novel novel;
+    private Document toc;
+
+    public betwixtedbutterfly_com() {
+    }
+
+    public betwixtedbutterfly_com(Novel novel) {
+        this.novel = novel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean canHeadless() {
+        return canHeadless;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public List<Chapter> getChapterList() {
+        List<Chapter> chapterList = new ArrayList();
+        try {
+            toc = Jsoup.connect(novel.novelLink)
+                    .cookies(novel.cookies)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            Elements chapterLinks = toc.select("div[id^=elementor-tab-content] a");
+            for (Element chapterLink : chapterLinks) {
+                chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
+            }
+        } catch (HttpStatusException httpEr) {
+            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+        } catch (IOException e) {
+            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+        } catch (NullPointerException e) {
+            GrabberUtils.err(novel.window, "Could not find expected selectors. Correct novel link?", e);
+        }
+        return chapterList;
+    }
+
+    public Element getChapterContent(Chapter chapter) {
+        Element chapterBody = null;
+        try {
+            Document doc = Jsoup.connect(chapter.chapterURL)
+                    .cookies(novel.cookies)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            chapterBody = doc.selectFirst("div.entry-inner");
+        } catch (HttpStatusException httpEr) {
+            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+        } catch (IOException e) {
+            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+        }
+        return chapterBody;
+    }
+
+    public NovelMetadata getMetadata() {
+        NovelMetadata metadata = new NovelMetadata();
+
+        if (toc != null) {
+            Element title = toc.selectFirst("h2.elementor-heading-title");
+            Element desc = toc.selectFirst("div.elementor-text-editor:not(:contains(Author))");
+            Element cover = toc.selectFirst(".elementor-image > img:nth-child(1)");
+
+            metadata.setTitle(title != null ? title.text() : "");
+            metadata.setDescription(desc != null ? desc.text() : "");
+            metadata.setBufferedCover(cover != null ? cover.attr("abs:src") : "");
+
+            Elements tags = toc.select("div.elementor-button-wrapper a");
+            List<String> subjects = new ArrayList<>();
+            for (Element tag : tags) {
+                subjects.add(tag.text());
+            }
+            metadata.setSubjects(subjects);
+        }
+
+        return metadata;
+    }
+
+    public List<String> getBlacklistedTags() {
+        List blacklistedTags = new ArrayList();
+        blacklistedTags.add("hr.wp-block-separator");
+        blacklistedTags.add("h2");
+        blacklistedTags.add("div.code-bloc");
+        blacklistedTags.add("div.wp-block-columns");
+        blacklistedTags.add("h3");
+        blacklistedTags.add("nav");
+        return blacklistedTags;
+    }
+
+}
