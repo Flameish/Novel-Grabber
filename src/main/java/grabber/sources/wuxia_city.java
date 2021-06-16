b9537e207d2d00e6f1d8ff71ba576b5422f746e7
Flameish <flameish@pm.me>
2021-06-16 12:37:45 +0200

    Added WuxiaCity

---
+++ src/main/java/grabber/sources/wuxia_city.java
@@ -0,0 +1,118 @@
+package grabber.sources;
+
+import grabber.Chapter;
+import grabber.GrabberUtils;
+import grabber.Novel;
+import grabber.NovelMetadata;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class wuxia_city implements Source {
+    private final String name = "WuxiaCity";
+    private final String url = "https://wuxia.city/";
+    private final boolean canHeadless = false;
+    private Novel novel;
+    private Document toc;
+
+    public wuxia_city() {
+    }
+
+    public wuxia_city(Novel novel) {
+        this.novel = novel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean canHeadless() {
+        return canHeadless;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public List<Chapter> getChapterList() {
+        List<Chapter> chapterList = new ArrayList();
+        try {
+            toc = Jsoup.connect(novel.novelLink)
+                    .cookies(novel.cookies)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            Document chapterPage = Jsoup.connect(toc.selectFirst(".book-tabs a:last-child").attr("abs:href")).get();
+            Elements chapterLinks = chapterPage.select("ul.chapters a");
+            for (Element chapterLink : chapterLinks) {
+                chapterLink.selectFirst("span.chapter-num").remove();
+                chapterList.add(new Chapter(chapterLink.selectFirst(".chapter-name").text(), chapterLink.attr("abs:href")));
+            }
+            Collections.reverse(chapterList);
+        } catch (HttpStatusException httpEr) {
+            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+        } catch (IOException e) {
+            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+        } catch (NullPointerException e) {
+            GrabberUtils.err(novel.window, "Could not find expected selectors. Correct novel link?", e);
+        }
+        return chapterList;
+    }
+
+    public Element getChapterContent(Chapter chapter) {
+        Element chapterBody = null;
+        try {
+            Document doc = Jsoup.connect(chapter.chapterURL)
+                    .cookies(novel.cookies)
+                    .userAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0")
+                    .get();
+            chapterBody = doc.selectFirst("div.chapter-content");
+        } catch (HttpStatusException httpEr) {
+            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+        } catch (IOException e) {
+            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+        }
+        return chapterBody;
+    }
+
+    public NovelMetadata getMetadata() {
+        NovelMetadata metadata = new NovelMetadata();
+
+        if (toc != null) {
+            Element title = toc.selectFirst("meta[property=og:title]");
+            Element author = toc.selectFirst("dl.author dd");
+            Element desc = toc.selectFirst(".book-synopsis");
+            Element cover = toc.selectFirst("meta[property=og:image]");
+
+            metadata.setTitle(title != null ? title.attr("content") : "");
+            metadata.setAuthor(author != null ? author.text() : "");
+            metadata.setDescription(desc != null ? desc.text().replace("Synopsis", "") : "");
+            metadata.setBufferedCover(cover != null ? cover.attr("abs:content") : "");
+
+            Elements tags = toc.select(".book-generes span");
+            List<String> subjects = new ArrayList<>();
+            for (Element tag : tags) {
+                subjects.add(tag.text());
+            }
+            metadata.setSubjects(subjects);
+        }
+
+        return metadata;
+    }
+
+    public List<String> getBlacklistedTags() {
+        List blacklistedTags = new ArrayList();
+        return blacklistedTags;
+    }
+
+}
