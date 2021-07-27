90ae99f7498974e8ec03d5455e85570152be97f5
Flameish <flameish@pm.me>
2021-07-27 21:31:03 +0200

    Version number

--- src/main/java/system/init.java
+++ src/main/java/system/init.java
@@ -22,7 +22,7 @@ import java.util.List;
  * Creates Library instance.
  */
 public class init {
-    public static final String versionNumber = "3.10.1";
+    public static final String versionNumber = "3.10.2";
     public static final Library library = Library.getInstance();;
     public static final Config config = Config.getInstance();
     public static GUI gui;
