a7046b68262926854e395da0d5dc0f00dadd91d8
KingOzriel <kingozriel@gmail.com>
2024-12-21 15:23:02 -0600

    novelfull_com.java

--- src/main/java/grabber/sources/biquge_se.java
+++ src/main/java/grabber/sources/biquge_se.java
@@ -1,100 +1,91 @@
 package grabber.sources;
 
-import grabber.Chapter;
-import grabber.GrabberUtils;
-import grabber.Novel;
-import grabber.NovelMetadata;
-import org.jsoup.HttpStatusException;
+import grabber.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class biquge_se implements Source {
-    private final String name = "Biquge";
-    private final String url = "http://www.biquge.se/";
-    private final boolean canHeadless = false;
-    private Novel novel;
+public class novelfull_com extends Source implements Initializable {
+    private final Novel novel;
     private Document toc;
+    private String bookTitle;
+    private String bookAuthor;
+    private String bookCover;
+    private String bookDesc;
+    private List<Chapter> chapters = new ArrayList<>();
+    private String nextChapterBtn = "";
+    private boolean reGrab = false;
 
-    public biquge_se(Novel novel) {
+    public novelfull_com(Novel novel) {
         this.novel = novel;
     }
 
-    public biquge_se() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean canHeadless() {
-        return canHeadless;
-    }
-
-    public String toString() {
-        return name;
-    }
-
-    public String getUrl() {
-        return url;
+    @Override
+    public void init() {
     }
 
     public List<Chapter> getChapterList() {
-        List<Chapter> chapterList = new ArrayList();
         try {
-            toc = Jsoup.connect(novel.novelLink).cookies(novel.cookies).get();
-            Elements chapterLinks = toc.select("#list dl dd:gt(10) a");
+            toc = Jsoup.connect(novel.novelLink).get();
+            Elements chapterLinks = toc.select(".list-chapter li a");
             for (Element chapterLink : chapterLinks) {
-                chapterList.add(new Chapter(chapterLink.text(), chapterLink.attr("abs:href")));
+                String chapterName = chapterLink.text();
+                String chapterUrl = chapterLink.attr("abs:href");
+                chapters.add(new Chapter(chapterName, chapterUrl));
             }
-        } catch (HttpStatusException httpEr) {
-            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
-        } catch (IOException e) {
-            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
-        } catch (NullPointerException e) {
-            GrabberUtils.err(novel.window, "Could not find expected selectors. Correct novel link?", e);
+        } catch (IOException | NullPointerException e) {
+            GrabberUtils.err(novel.window, "Could not get chapter list from: " + novel.novelLink, e);
         }
-        return chapterList;
+        return chapters;
     }
 
-    public Element getChapterContent(Chapter chapter) {
-        Element chapterBody = null;
+    public String getChapterContent(Chapter chapter) {
+        Element chapterContent = null;
         try {
-            Document doc = Jsoup.connect(chapter.chapterURL).cookies(novel.cookies).get();
-            chapterBody = doc.select("#content").first();
-        } catch (HttpStatusException httpEr) {
-            GrabberUtils.err(novel.window, GrabberUtils.getHTMLErrMsg(httpEr));
+            Document doc = Jsoup.connect(chapter.chapterURL).get();
+            chapterContent = doc.select("#chapter-content").first();
         } catch (IOException e) {
-            GrabberUtils.err(novel.window, "Could not connect to webpage!", e);
+            GrabberUtils.err(novel.window, "Could not get chapter content from: " + chapter.chapterURL, e);
         }
-        return chapterBody;
+        return chapterContent != null ? chapterContent.toString() : "";
     }
 
-    public NovelMetadata getMetadata() {
-        NovelMetadata metadata = new NovelMetadata();
-
-        if (toc != null) {
-            metadata.setTitle(toc.selectFirst("meta[property=og:title]").attr("content"));
-            metadata.setAuthor(toc.selectFirst("meta[property=og:novel:author]").attr("content"));
-            metadata.setDescription(toc.selectFirst("#intro").text());
-            metadata.setBufferedCover(toc.selectFirst("meta[property=og:image]").attr("content"));
-
-            List<String> subjects = new ArrayList<>();
-            subjects.add(toc.select("meta[property=og:novel:category]").attr("content"));
-            metadata.setSubjects(subjects);
+    public void getNovelMetadata() {
+        if (toc == null) {
+            GrabberUtils.err("Table of contents not loaded. Cannot get novel metadata.");
+            return;
         }
-
-        return metadata;
+        Element titleElement = toc.select(".book h3").first();
+        bookTitle = titleElement != null ? titleElement.text() : "";
+        Element authorElement = toc.select(".info a[href*=author]").first();
+        bookAuthor = authorElement != null ? authorElement.text() : "";
+        Element descElement = toc.select(".desc-text").first();
+        bookDesc = descElement != null ? descElement.text() : "";
+        Element coverElement = toc.select(".book img").first();
+        bookCover = coverElement != null ? coverElement.attr("abs:src") : "";
+        novel.setMetadata(bookTitle, bookAuthor, bookCover, null, bookDesc);
     }
 
+    @Override
     public List<String> getBlacklistedTags() {
-        List blacklistedTags = new ArrayList();
+        List<String> blacklistedTags = new ArrayList<>();
+        blacklistedTags.add("script");
+        blacklistedTags.add("style");
         return blacklistedTags;
     }
 
+    @Override
+    public Map<String, String> getCookies() {
+        return null;
+    }
 }
